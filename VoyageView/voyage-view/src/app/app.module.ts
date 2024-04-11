cee0a2fd0a6415e31d82ea1b965064e57f74a56d
Sevgy <121260146+Sevgy@users.noreply.github.com>
2024-04-11 12:54:07 +0300

    included Home and NotFound components

--- VoyageView/voyage-view/src/app/app.module.ts
+++ VoyageView/voyage-view/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [BrowserModule, CoreModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
